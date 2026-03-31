fb3d61e30da718469f9407645a8fd3888e9be671
Danila Pachkov <inversopro@yandex.ru>
2026-03-31 11:44:16 +0300

    update scanner

--- api/send.js
+++ api/send.js
@@ -1,30 +1,25 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ ok: false });
+    return res.status(405).json({ ok: false, error: 'Method Not Allowed' });
   }
 
   const { text } = req.body;
-
   const TG_TOKEN = process.env.TG_TOKEN;
-  const CHAT_IDS = process.env.TG_CHAT_IDS.split(',');
+  const TG_CHAT_IDS = JSON.parse(process.env.TG_CHAT_IDS);
 
   try {
     await Promise.all(
-      CHAT_IDS.map(chat_id =>
+      TG_CHAT_IDS.map(chat_id =>
         fetch(`https://api.telegram.org/bot${TG_TOKEN}/sendMessage`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            chat_id,
-            text,
-            parse_mode: 'HTML'
-          })
+          body: JSON.stringify({ chat_id, text, parse_mode: 'HTML' })
         })
       )
     );
-
-    return res.status(200).json({ ok: true });
-  } catch (e) {
-    return res.status(500).json({ ok: false });
+    res.status(200).json({ ok: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ ok: false });
   }
 }
